6a26221c1afb1a91e7a7ce9d98d2a24f70af0a49
Amber Sautner <ambersautner3@gmail.com>
2023-11-27 16:16:30 -0500

    updated flask web app

--- app/__init__.py
+++ app/__init__.py
@@ -1,171 +1,57 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+"""A simple flask web app"""
+import flask_login
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+from flask_wtf.csrf import CSRFProtect
 
-from app.auth.decorators import admin_required
-from flask_login import login_user, login_required, logout_user, current_user
-from sqlalchemy.orm import load_only
-from werkzeug.security import generate_password_hash
-from app.auth.forms import login_form, register_form, profile_form, security_form, user_edit_form
-from app.db import db
+import os
+from flask import Flask
+from app.context_processors import utility_text_processors
+from app.simple_pages import simple_pages
+from app.auth import auth
+from app.exceptions import http_exceptions
 from app.db.models import User
-
-auth = Blueprint('auth', __name__, template_folder='templates')
-from flask import current_app
-
-
-@auth.route('/login', methods=['POST', 'GET'])
-def login():
-    form = login_form()
-    if current_user.is_authenticated:
-        return redirect(url_for('auth.dashboard'))
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('auth.login'))
-        else:
-            user.authenticated = True
-            db.session.add(user)
-            db.session.commit()
-            login_user(user)
-            flash("Welcome", 'success')
-            return redirect(url_for('auth.dashboard'))
-    return render_template('login.html', form=form)
+from app.db import db
+from app.auth import auth
+from app.cli import create_database
 
 
-@auth.route('/register', methods=['POST', 'GET'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('auth.dashboard'))
-    form = register_form()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user is None:
-            user = User(email=form.email.data, password=generate_password_hash(form.password.data))
-            db.session.add(user)
-            db.session.commit()
-            if user.id == 1:
-                user.is_admin = 1
-                db.session.add(user)
-                db.session.commit()
-            flash('Congratulations, you are now a registered user!', "success")
-            return redirect(url_for('auth.login'), 302)
-        else:
-            flash('Already Registered')
-            return redirect(url_for('auth.login'), 302)
-    return render_template('register.html', form=form)
+login_manager = flask_login.LoginManager()
 
 
-@auth.route('/dashboard')
-@login_required
-def dashboard():
-    return render_template('dashboard.html')
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 
-@auth.route("/logout")
-@login_required
-def logout():
-    """Logout the current user."""
-    user = current_user
-    user.authenticated = False
-    db.session.add(user)
-    db.session.commit()
-    logout_user()
-    return redirect(url_for('auth.login'))
+def create_app():
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__)
+    app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
+    csrf = CSRFProtect(app)
+    bootstrap = Bootstrap5(app)
+    app.register_blueprint(simple_pages)
+    app.register_blueprint(auth)
+    app.context_processor(utility_text_processors)
+    app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'Simplex'
+    app.register_error_handler(404, page_not_found)
+    # app.add_url_rule("/", endpoint="index")
+    db_dir = "database/db.sqlite"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    # add command function to cli commands
+    app.cli.add_command(create_database)
+    # Setup Flask-User and specify the User data-model
+
+    return app
 
 
-@auth.route('/users')
-@login_required
-@admin_required
-def browse_users():
-    current_app.logger.info('Info level log')
-    current_app.logger.warning('Warning level log')
-    data = User.query.all()
-    titles = [('email', 'Email'), ('registered_on', 'Registered On')]
-    retrieve_url = ('auth.retrieve_user', [('user_id', ':id')])
-    edit_url = ('auth.edit_user', [('user_id', ':id')])
-    add_url = url_for('auth.add_user')
-    delete_url = ('auth.delete_user', [('user_id', ':id')])
-    return render_template('browse.html', titles=titles, add_url=add_url, edit_url=edit_url, delete_url=delete_url,
-                           retrieve_url=retrieve_url, data=data, User=User, record_type="Users")
-
-
-@auth.route('/users/<int:user_id>')
-@login_required
-def retrieve_user(user_id):
-    user = User.query.get(user_id)
-    return render_template('profile_view.html', user=user)
-
-
-@auth.route('/users/<int:user_id>/edit', methods=['POST', 'GET'])
-@login_required
-def edit_user(user_id):
-    user = User.query.get(user_id)
-    form = user_edit_form(obj=user)
-    if form.validate_on_submit():
-        user.about = form.about.data
-        user.is_admin = int(form.is_admin.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('User Edited Successfully', 'success')
-        return redirect(url_for('auth.browse_users'))
-    return render_template('user_edit.html', form=form)
-
-
-@auth.route('/users/new', methods=['POST', 'GET'])
-@login_required
-def add_user():
-    form = register_form()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user is None:
-            user = User(email=form.email.data, password=generate_password_hash(form.password.data))
-            db.session.add(user)
-            db.session.commit()
-            flash('Congratulations, you just created a user', 'success')
-            return redirect(url_for('auth.browse_users'))
-        else:
-            flash('Already Registered')
-            return redirect(url_for('auth.browse_users'))
-    return render_template('user_new.html', form=form)
-
-
-@auth.route('/users/<int:user_id>/delete', methods=['POST'])
-@login_required
-def delete_user(user_id):
-    user = User.query.get(user_id)
-    if user.id == current_user.id:
-        flash("You can't delete yourself!")
-        return redirect(url_for('auth.browse_users'), 302)
-    db.session.delete(user)
-    db.session.commit()
-    flash('User Deleted', 'success')
-    return redirect(url_for('auth.browse_users'), 302)
-
-
-@auth.route('/profile', methods=['POST', 'GET'])
-def edit_profile():
-    user = User.query.get(current_user.get_id())
-    form = profile_form(obj=user)
-    if form.validate_on_submit():
-        user.about = form.about.data
-        db.session.add(current_user)
-        db.session.commit()
-        flash('You Successfully Updated your Profile', 'success')
-        return redirect(url_for('auth.dashboard'))
-    return render_template('profile_edit.html', form=form)
-
-
-@auth.route('/account', methods=['POST', 'GET'])
-def edit_account():
-    user = User.query.get(current_user.get_id())
-    form = security_form(obj=user)
-    if form.validate_on_submit():
-        user.email = form.email.data
-        user.password = form.password.data
-        db.session.add(current_user)
-        db.session.commit()
-        flash('You Successfully Updated your Password or Email', 'success')
-        return redirect(url_for('auth.dashboard'))
-    return render_template('manage_account.html', form=form)
-
-
+@login_manager.user_loader
+def user_loader(user_id):
+    try:
+        return User.query.get(int(user_id))
+    except:
+        return None
